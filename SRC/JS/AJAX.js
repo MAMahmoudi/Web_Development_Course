84111b2baa9cea14fe0be40a05d14e048b87cc7f
Mahmoudi Mohamed Amine <mohamed.mahmoudi@ensia.edu.dz>
2024-12-31 14:08:07 +0100

    ajax

--- SRC/JS/AJAX.js
+++ SRC/JS/AJAX.js
@@ -20,18 +20,22 @@ xhttp.send();
 //////////////////////////////////////////////////////////////////////
 var xhttp5 = new XMLHttpRequest();
 function Start() {
-  window.setInterval(Repeate, 2000);
-  function Repeate() {
-    console.log(xhttp5.responseText);
-    xhttp5.onreadystatechange = function () {
-      if (xhttp5.readyState == 4 && xhttp5.status == 200) {
-        document.getElementById("demo3").innerHTML +=
-          xhttp5.responseText + "<br>";
-      }
-    };
-    xhttp5.open("GET", "SRC/JS/My_File3.txt", true);
-    xhttp5.send();
-  }
+  console.log("Start 1");
+  xhttp5.onreadystatechange = function () {
+    if (xhttp5.readyState == 4 && xhttp5.status == 200) {
+      console.log("Start");
+      document.getElementById("demo3").innerHTML += "Opening<br>";
+      document.getElementById("demo3").innerHTML +=
+        xhttp5.responseText + "<br>";
+    }
+  };
+  sleep(2000);
+  xhttp5.open("GET", "SRC/JS/My_File3.txt", true);
+  xhttp5.send();
+}
+
+function Opening() {
+  document.getElementById("demo3").innerHTML += "Waisting time <br>";
 }
 
 function abortFunction() {
